6acbd8fbdfadb7a717b2b669259dfc6cf1b0f05f
Dan Habot <dan.habot@gmail.com>
2019-10-12 23:49:17 -0400

    refactor

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ program.version(require('./package.json').version)
 program.command('listen <port>')
   .description('Listen for websocket connections on a port')
   .action(port => {
-    // TODO: Start websocket server here I guess
-    console.log(port)
+    const wss = new WebSocket.Server({ port: port })
+    var counter = 0
+ 
+    wss.on('connection', function connection(ws) {
+      ws.number = counter++
+      ws.on('message', function incoming(data) {
+        
+      });
+    });
   })
 program.command('connect <address>')
   .description('Connect to a websocket at an address')
@@ -27,45 +34,46 @@ program.command('connect <address>')
     })
 
     process.stdin.on('keypress', (ch, key) => {
-      if (key && key.ctrl && key.name == 'c') {
-        ws.close()
-        process.stdin.pause();
-        process.exit(0)
-        return
-      }
-
-      if (outOn && key && key.name == 'r') {
-        process.stdin.pause()
-        outOn = false;
-        process.stdout.write('<<< ')
-
-        process.stdin.resume()
-        return
-      }
-
-      if (!outOn) {
-        if (key && key.ctrl && key.name == 'd') {
-          process.stdout.write('\r')
-          outOn = true;
+      if (key) {
+        if (key.ctrl && key.name == 'c') {
+          ws.close()
+          process.stdin.pause();
+          process.exit(0)
+          return
+        }
+        
+        if (outOn && key.name == 'r') {
+          process.stdin.pause()
+          outOn = false;
+          process.stdout.write('<<< ')
+  
+          process.stdin.resume()
           return
         }
 
-        // TODO: Factor out the boolean expressions
-        if (key && key.name == 'return') {
-          process.stdout.write('\n')
-          outOn = true; // TODO: Make auto-outon a setting
-          ws.send(JSON.stringify(buffer))
-          buffer = ""
-          keypress(process.stdin);
-          return
+        if (!outOn) {
+          if (key.ctrl && key.name == 'd') {
+            process.stdout.write('\r')
+            outOn = true;
+            return
+          }
+  
+          // TODO: Factor out the boolean expressions
+          if (key.name == 'return') {
+            process.stdout.write('\n')
+            outOn = true; // TODO: Make auto-outon a setting
+            ws.send(JSON.stringify(buffer))
+            buffer = ""
+            keypress(process.stdin)
+            return
+          }
+  
+          if (key.name == 'backspace') {
+            buffer = buffer.slice(0, -1)
+            process.stdout.write('\b \b')
+            return
+          }
         }
-
-        if (key && key.name == 'backspace') {
-          buffer = buffer.slice(0, -1)
-          process.stdout.write('\b \b')
-          return
-        }
-
         buffer += ch
         process.stdout.write(ch)
       }
